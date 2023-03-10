551a758effb9c2300654fe5f858c2815c7c08777
brainchemist2005 <zackariaebouargan@gmail.com>
2023-03-10 00:45:03 -0500

    Updated Main work in progess

--- Main.java
+++ Main.java
@@ -10,6 +10,51 @@
 
 public class Main {
 	
+	public static String cryptage()
+	{
+		int i = 0;
+		String Cle,Message;
+		
+		System.out.println("++++++++++++\n"
+				+ "+ CRYPTAGE +\n"
+				+ "++++++++++++");
+		
+		System.out.println("Cle DE CRYPTAGE : ");
+		Cle = Clavier.lireString();
+		Cle.toUpperCase();
+		
+		if(Cle.length()%4 != 0)
+		{
+			System.out.println("ERREUR, Cle invalide ! Recommencez...");
+		}
+		
+		while(i < Cle.length()/4)
+		{
+			if(Cle.charAt(0) == 'P' && Cle.charAt(1) == 'I')
+			{
+				Message = PermutationInterieure(Cle,Message);
+			}
+			else if(Cle.charAt(0) == 'I' && Cle.charAt(1) == 'V')
+			{
+				Message = Inversion(Cle,Message);
+			}
+			else if(Cle.charAt(0) == 'P' && Cle.charAt(1) == 'E')
+			{
+				Message = PermutationExterieure(Cle,Message);
+			}
+			else if(Cle.charAt(0) == 'R' && Cle.charAt(1) == 'D')
+			{
+				Message = RotationDroite(Cle,Message);			
+			}
+			else if(Cle.charAt(0) == 'R' && Cle.charAt(1) == 'G')
+			{
+				Message = RotationGauche(Cle,Message);
+			}
+			i++;
+		}
+		return Message;
+	}
+	
 	public static void menu()
 	{
 		System.out.println("----\n"
@@ -23,7 +68,7 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-		int choix;
+		char choix;
 		
 		System.out.println("Ce logiciel permet de crypter et de decrypter des messages secrets.\n"
 				+ "\n"
@@ -33,11 +78,29 @@ public class Main {
 		{
 			menu();
 			
-			choix = Clavier.lireInt();
-			if(choix < 1 || choix > 3)
-				System.out.println("ERREUR, choix de menu invalide ! Recommencez...");
-		}
-		while(choix < 1 || choix > 3);
+			choix = Clavier.lireCharLn();
+			
+			switch(choix)
+			{
+				case '1':
+					cryptage();
+					
+					break;
+					
+				case '2':
+					
+					break;
+					
+				case '3':
+					System.out.println("Vous avez décidé de quitter.");
+					break;
+										
+				default: 
+					System.out.println("ERREUR, choix de menu invalide ! Recommencez...");
+			}
+			
+			
+		}while(choix != '3');
 		
 	}
 
